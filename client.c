475aa6a3b112feb10e3e9ca065c1616d8541f059
Alexander Carter <alexander.carterhp@stu.austinisd.org>
2018-02-22 14:07:10 -0600

    client.c

---
+++ client.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <sys/socket.h>
+#include <stdlib.h>
+#include <netinet/in.h>
+#include <string.h>
+#define PORT 17425
+
+int main(int argc, char** argv) {
+	int server_fd, new_socket, valread;
+	if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+		perror("socket failed");
+		exit(EXIT_FAILURE);
+	}
+	return 0;
+}
